13a6193a8cce1b6ae54b9364eee11f14907b6a95
Роман Ким <143472757+Romchanka@users.noreply.github.com>
2023-11-30 16:15:47 +0600

    Phone pattern added

--- src/main/java/com/example/shop/Config/PatternConfig.java
+++ src/main/java/com/example/shop/Config/PatternConfig.java
@@ -6,6 +6,7 @@ public class PatternConfig {
     public static final Pattern
     EMPLOYEE_NAME_PATTERN = Pattern.compile("^[A-Za-zA-Яа-я]{2,50}$"),
     PASSWORD_PATTERN=Pattern.compile("^(?=.*[A-Za-z\\d])(?=.*[^A-Za-z\\d]).{8,32}$"),
+    PHONE_PATTERN= Pattern.compile("^\\+996\\d{9}$"),
     EMAIL_PATTERN= Pattern.compile("^[A-Za-z\\d+_.-]+@(.+)$"),
     DATE_PATTERN =Pattern.compile("^(|(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(\\d{4}))$");
 }
